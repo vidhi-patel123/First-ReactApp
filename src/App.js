3b14d90aac903fdff73eaea48a1774f001d7114e
Vidhi Patel <patelvidhi8420@gmail.com>
2026-05-12 18:15:49 +0530

    Create Navbar, Textform and Aboutus and also add Darkmode with use of proper ReactJs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import { use, useState } from 'react';
 import './App.css';
+import About from './components/About';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
 
 function App() {
+  const [mode, setMode] = useState("light"); // dark mode is enabld or not
+  
+  const toggleMode = ()=>{
+    if(mode === "light"){
+      setMode("dark");
+      // document.body.style.backgroundColor = "#042743";
+    } else {
+      setMode("light");
+      // document.body.style.backgroundColor = "white";
+    }
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>  
+      {/* <Navbar title="TextUtils" aboutText="About Us" /> */}
+      {/* <Navbar title="TextUtils" /> */}
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
+      <div className="container my-3">
+      <TextForm heading="Enter text to analyze" />
+      </div>
+      <About/>
+    </>
   );
 }
 
---
+++ src/components/TextForm.js
@@ -0,0 +1,41 @@
+import React, {useState} from 'react'
+
+
+export default function TextForm(props) {
+    const handleUpClick = () => {
+        // console.log("Uppercase was clicked" + text);
+        let newText = text.toUpperCase();
+        setText(newText);
+    }
+
+    const handleLoClick = () => {
+        // console.log("Lowercase was clicked" + text);
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
+
+    const handleOnchange = (event)=> {
+        // console.log("On change");
+        setText(event.target.value);
+    }
+
+    const [text, setText] = useState('Enter text here2');
+    return (
+        <>
+        <div className='container'>
+            <h3>{props.heading}</h3>
+            <div className="mb-3">
+                <textarea value={text} onChange={handleOnchange} id="myBox" className="form-control" />
+            </div>           
+            <button type="submit" className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
+            <button type="submit" className="btn btn-secondary mx-1" onClick={handleLoClick}>Convert to Lowercase</button>
+        </div>
+        <div className="container my-3">
+            <h2>Your text summary</h2>
+            <p>{text.split(" ").length} words and {text.length} characters</p>
+            <h3>Preview</h3>
+            <p>{text.length>0?text: "Enter something to preview"}</p>
+        </div>
+        </>
+    )
+    }
