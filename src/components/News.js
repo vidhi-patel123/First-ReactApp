53966e4743526eba7f994da15434aab8db9ec36d
Vidhi Patel <patelvidhi8420@gmail.com>
2026-05-14 17:45:48 +0530

    Call all API's and Previous and next buttons working

--- src/components/News.js
+++ src/components/News.js
@@ -2,59 +2,64 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
-  articles= [
-    {
-      "source": {
-        "id": "the-washington-post",
-        "name": "The Washington Post"
-      },
-      "author": "Rick Maese",
-      "title": "Jason Collins, a gay trailblazer in the NBA, dies of brain cancer at 47 - The Washington Post",
-      "description": "He was the first active, openly gay player in the league’s history. In December, he announced that he had a deadly form of brain cancer.",
-      "url": "https://www.washingtonpost.com/obituaries/2026/05/12/jason-collins-first-openly-gay-nba-player-dies-brain-cancer-47/",
-      "urlToImage": "https://www.washingtonpost.com/wp-apps/imrs.php?src=https://cloudfront-us-east-1.images.arcpublishing.com/wapo/BIU6VVCGF4VLYHURFJ7CE6D62I.jpg&w=1440",
-      "publishedAt": "2026-05-13T02:01:29Z",
-      "content": "For most of his 13 seasons in the National Basketball Association, Jason Collins was known as the sort of player contenders always seemed to need and casual fans often overlooked a broad-shouldered, … [+218 chars]"
-    },
-    {
-      "source": {
-        "id": null,
-        "name": "Abcnews.com"
-      },
-      "author": "The Associated Press",
-      "title": "From Dani Rojas to the USL: Cristo Fernandez earns an El Paso Locomotive deal - ABC News - Breaking News, Latest News and Videos",
-      "description": "Forward Cristo Fernandez, the actor known who portrayed Dani Rojas on the Apple TV series “Ted Lasso,” has signed with El Paso Locomotive FC of the USL to play soccer professionally",
-      "url": "https://abcnews.com/Entertainment/wireStory/dani-rojas-usl-cristo-fernandez-earns-el-paso-132889386",
-      "urlToImage": "https://i.abcnewsfe.com/a/13ed2d75-a226-4e3b-8c96-f2515e7f3e69/wirestory_66cf925389011fd6deb8565fcd2c12c0_16x9.jpg?w=1600",
-      "publishedAt": "2026-05-12T19:12:19Z",
-      "content": "EL PASO, Texas -- Forward Cristo Fernandez, the actor who portrayed Dani Rojas on the Apple TV series Ted Lasso has signed with El Paso Locomotive FC of the USL to play soccer professionally.\r\nTerms … [+849 chars]"
-    }
-  ]
+  
   constructor() {
     super();
-    console.log("Hello, I am constructor from news")
     this.state = {
-      articles: this.articles,
-      loading: false
+      articles: [],
+      loading: false,
+      page:1
       
   }  
 }
 
+async componentDidMount() {
+    let url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=0670e6d3adf8410db649cd9ef5f85440&page=1&pageSize=20";
+    let response = await fetch(url);
+    let data = await response.json();
+    this.setState({ articles: data.articles, loading: false });
+}
+
+handlePrevClick = async () => {
+  console.log("Previous");
+  let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=0670e6d3adf8410db649cd9ef5f85440&page=${this.state.page - 1}`;
+  let response = await fetch(url);
+  let data = await response.json();
+  this.setState({
+    page: this.state.page - 1,
+    articles: data.articles,
+  });
+}
+
+handleNextClick = async () => {
+  console.log("Next");
+  let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=0670e6d3adf8410db649cd9ef5f85440&page=${this.state.page + 1}`;
+  let response = await fetch(url);
+  let data = await response.json();
+  this.setState({
+    page: this.state.page + 1,
+    articles: data.articles,
+  });
+}
   render() {
     return (
       <div className="container my-3">
         <h2>Latest News Monkey Headlines</h2>
         <div className="row">
-          <div className="col-md-4">
-            <NewsItem title="News about Play " description="This is a Play description." imageurl="https://www.washingtonpost.com/wp-apps/imrs.php?src=https://cloudfront-us-east-1.images.arcpublishing.com/wapo/BIU6VVCGF4VLYHURFJ7CE6D62I.jpg&w=1440" newsUrl="https://www.washingtonpost.com/obituaries/2026/05/12/jason-collins-first-openly-gay-nba-player-dies-brain-cancer-47/" />
-          </div>
-          <div className="col-md-4">
-            <NewsItem title="Another News Title" description="This is another news description." imageurl="https://www.washingtonpost.com/wp-apps/imrs.php?src=https://cloudfront-us-east-1.images.arcpublishing.com/wapo/BIU6VVCGF4VLYHURFJ7CE6D62I.jpg&w=1440" newsUrl="https://abcnews.com/Entertainment/wireStory/dani-rojas-usl-cristo-fernandez-earns-el-paso-132889386"  />
-          </div>
-          <div className="col-md-4">
-            <NewsItem title="Another News Title" description="This is another news description." imageurl="https://www.washingtonpost.com/wp-apps/imrs.php?src=https://cloudfront-us-east-1.images.arcpublishing.com/wapo/BIU6VVCGF4VLYHURFJ7CE6D62I.jpg&w=1440" newsUrl="https://www.washingtonpost.com/obituaries/2026/05/12/jason-collins-first-openly-gay-nba-player-dies-brain-cancer-47/" />
-          </div>
+          {this.state.articles.map((element) => {
+            return <div className="col-md-4" key={element.url}>
+              <NewsItem title={element.title?element.title:""} description={element.description?element.description:""} imageurl={element.urlToImage} newsUrl={element.url} />
+            </div>
+          })}
         </div>
+        <div className="container my-3 justify-content-between d-flex">
+
+  <button className="btn btn-dark" disabled={this.state.page <= 1} onClick={this.handlePrevClick}>Previous</button>
+
+  <button className="btn btn-dark" onClick={this.handleNextClick}>Next
+  </button>
+
+</div>
       </div>
     )
   }
